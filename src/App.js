b09baae4d7de8f5f5097c35479ebac3fffe9599e
edgard2 <mve.michel@gmail.fr>
2019-12-07 07:04:23 +0100

    Fin de la première partie

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Header from './Components/Header';
+import Sidebar from './Components/SideBar';
+
+
+import ShowEarning from './Components/ShowEarning';
+import Connexion from './Components/Connexion';
+import Dashboard from './Components/Dashboard';
+import Footer from './Components/Footer';
+import EditAccount from "./Components/EditAccount";
+
+import ShowUsers from "./Components/ShowUsers";
+
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+
+class App extends React.Component{
+    render(){
+        return (<BrowserRouter>
+
+                <Header/>
+                <Sidebar/>
+                <div>
+                    <Switch>
+                        <Route path="/dashboard" component={Dashboard}/>
+                        <Route path="/connexion" component={Connexion}/>
+                        <Route path="/show-earning" component={ShowEarning}/>xx
+                        <Route path="/edit-account" component={EditAccount}/>
+                        <Route path="/show-users" component={ShowUsers}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/register" component={Register}/>
+
+                    </Switch>
+                </div>
+                <div>
+                <Footer/>
+            </div>
+        </BrowserRouter>
+        )
+    }
 }
 
-export default App;
+export default App
